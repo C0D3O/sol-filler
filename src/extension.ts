9b9f7ce73e692beedae807962a0518a3d559dbdf
1111Eugene <1111eugene.eugene@gmail.com>
2023-06-25 13:20:56 +0300

    fixed activation

--- src/extension.ts
+++ src/extension.ts
@@ -1,35 +1,31 @@
 import * as vscode from 'vscode';
 
-export function activate(context: vscode.ExtensionContext) {
-	const disposable = vscode.commands.registerCommand('solfiller.watch', () => {
-		try {
-			const folders = vscode.workspace.workspaceFolders![0].uri.path;
+export function activate() {
+	try {
+		const folders = vscode.workspace.workspaceFolders![0].uri.path;
 
-			if (folders) {
-				const watcher = vscode.workspace.createFileSystemWatcher(new vscode.RelativePattern(folders, '**/*.sol'));
+		if (folders) {
+			const watcher = vscode.workspace.createFileSystemWatcher(new vscode.RelativePattern(folders, '**/*.sol'));
 
-				watcher.onDidCreate(uri => {
-					const path = uri.path.slice(1);
-					const fileNameSplit = path.split('/').at(-1);
-					if (fileNameSplit) {
-						const firstLetterToUpperCase = fileNameSplit.charAt(0).toUpperCase();
-						const fileName = firstLetterToUpperCase + fileNameSplit.slice(1, -4);
-						const snippet = new vscode.SnippetString(`// SPDX-License-Identifier: MIT
+			watcher.onDidCreate(uri => {
+				const path = uri.path.slice(1);
+				const fileNameSplit = path.split('/').at(-1);
+				if (fileNameSplit) {
+					const firstLetterToUpperCase = fileNameSplit.charAt(0).toUpperCase();
+					const fileName = firstLetterToUpperCase + fileNameSplit.slice(1, -4);
+					const snippet = new vscode.SnippetString(`// SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 										
 contract ${fileName} {
 	$1
 }`);
-						vscode.window.activeTextEditor?.insertSnippet(snippet);
-					}
-				});
-			}
-		} catch (error) {
-			console.log(error);
+					vscode.window.activeTextEditor?.insertSnippet(snippet);
+				}
+			});
 		}
-	});
-
-	context.subscriptions.push(disposable);
+	} catch (error) {
+		console.log(error);
+	}
 }
 
 export function deactivate() {}
