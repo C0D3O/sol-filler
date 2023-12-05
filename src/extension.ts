51c498c1bfaae184a3e6f97cbe8d2bc64f1f5eba
moneyDev1111 <1111eugene.eugene@gmail.com>
2023-12-05 15:33:35 +0200

    upd

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,11 @@
-import * as vscode from 'vscode';
+import { RelativePattern, SnippetString, window, workspace } from 'vscode';
 
 export function activate() {
 	try {
-		const folders = vscode.workspace.workspaceFolders![0].uri.path;
+		const folders = workspace.workspaceFolders![0].uri.path;
 
 		if (folders) {
-			const watcher = vscode.workspace.createFileSystemWatcher(new vscode.RelativePattern(folders, '**/*.sol'));
+			const watcher = workspace.createFileSystemWatcher(new RelativePattern(folders, '**/*.sol'));
 
 			watcher.onDidCreate((uri) => {
 				const path = uri.path.slice(1);
@@ -13,21 +13,16 @@ export function activate() {
 				if (fileNameSplit) {
 					const firstLetterToUpperCase = fileNameSplit.charAt(0).toUpperCase();
 					const fileName = firstLetterToUpperCase + fileNameSplit.slice(1, -4);
-					const snippet = new vscode.SnippetString(`// SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.21;
+					const snippet = new SnippetString(`// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.23;
 
 contract ${fileName} {
 		$1
 }`);
-					vscode.window.activeTextEditor?.document.fileName!.split('.').pop() === 'sol' && !vscode.window.activeTextEditor?.document.getText()
-						? vscode.window.activeTextEditor?.insertSnippet(snippet)
+					window.activeTextEditor?.document.fileName!.split('.').pop() === 'sol' && !window.activeTextEditor?.document.getText()
+						? window.activeTextEditor?.insertSnippet(snippet)
 						: null;
 				}
-			});
-			// const textWatcher = vscode.workspace.createFileSystemWatcher(new vscode.RelativePattern(folders, '**/*.sol'));
-
-			watcher.onDidChange((uri) => {
-				console.log(uri.path);
 			});
 		}
 	} catch (error) {
